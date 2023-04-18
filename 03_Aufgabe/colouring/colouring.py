0f574f8984bd67e3aa8ab9a5ff855c46318f9afb
Smokey95 <simon.publicmail@gmail.com>
2023-04-18 19:20:15 +0200

    Add first not working version of colouring problem

--- 03_Aufgabe/colouring/colouring.py
+++ 03_Aufgabe/colouring/colouring.py
@@ -10,15 +10,15 @@
 #
 # HINTS
 #
-# 1. Baden-Württemberg has a border to Hessen, Bayern and Rheinland-Pfalz.
-# 2. Bayern has a border to Baden-Württemberg, Hessen, Thüringen and Sachsen.
-# 3. Berlin has a border to Brandenburg.
-# 4. Brandenburg has a border to Berlin, Mecklenburg-Vorpommern, Niedersachsen, Sachsen-Anhalt and Sachsen.
-# 5. Bremen has a border to Niedersachsen.
-# 6. Hamburg has a border to Niedersachsen and Schleswig-Holstein.
-# 7. Hessen has a border to Baden-Württemberg, Bayern, Rheinland-Pfalz, Nordrhein-Westfalen, Thüringen and Niedersachsen.
-# 8. Mecklenburg-Vorpommern has a border to Brandenburg, Niedersachsen and Schleswig-Holstein.
-# 9. Niedersachsen has a border to Bremen, Hamburg, Nordrhein-Westfalen, Mecklenburg-Vorpommern, Sachsen-Anhalt, Schleswig-Holstein, Thüringen and Hessen.
+# 1.  Baden-Württemberg has a border to Hessen, Bayern and Rheinland-Pfalz.
+# 2.  Bayern has a border to Baden-Württemberg, Hessen, Thüringen and Sachsen.
+# 3.  Berlin has a border to Brandenburg.
+# 4.  Brandenburg has a border to Berlin, Mecklenburg-Vorpommern, Niedersachsen, Sachsen-Anhalt and Sachsen.
+# 5.  Bremen has a border to Niedersachsen.
+# 6.  Hamburg has a border to Niedersachsen and Schleswig-Holstein.
+# 7.  Hessen has a border to Baden-Württemberg, Bayern, Rheinland-Pfalz, Nordrhein-Westfalen, Thüringen and Niedersachsen.
+# 8.  Mecklenburg-Vorpommern has a border to Brandenburg, Niedersachsen and Schleswig-Holstein.
+# 9.  Niedersachsen has a border to Bremen, Hamburg, Nordrhein-Westfalen, Mecklenburg-Vorpommern, Sachsen-Anhalt, Schleswig-Holstein, Thüringen and Hessen.
 # 10. Nordrhein-Westfalen has a border to Hessen, Niedersachsen, Rheinland-Pfalz.
 # 11. Rheinland-Pfalz has a border to Baden-Württemberg, Hessen, Nordrhein-Westfalen, Saarland.
 # 12. Saarland has a border to Rheinland-Pfalz.
@@ -29,3 +29,94 @@
 #
 
 from constraint import Problem, AllDifferentConstraint
+
+def solve(limit):
+    
+    # define the variables
+    states = ["Baden-Württemberg", 
+              "Bayern", 
+              "Berlin", 
+              "Brandenburg", 
+              "Bremen", 
+              "Hamburg", 
+              "Hessen", 
+              "Mecklenburg-Vorpommern", 
+              "Niedersachsen", 
+              "Nordrhein-Westfalen", 
+              "Rheinland-Pfalz", 
+              "Saarland", 
+              "Sachsen", 
+              "Sachsen-Anhalt", 
+              "Schleswig-Holstein", 
+              "Thüringen"]
+    # Why is Mallorca missing?
+    
+    problem = Problem()
+    
+    # define the domains
+    problem.addVariables(states, range(1, limit + 1))
+    
+    # define the constraints
+    
+    # Hint 1
+    problem.addConstraint(AllDifferentConstraint(), ["Baden-Württemberg", "Hessen", "Bayern", "Rheinland-Pfalz"])
+    
+    # Hint 2
+    problem.addConstraint(AllDifferentConstraint(), ["Bayern", "Baden-Württemberg", "Hessen", "Thüringen", "Sachsen"])
+    
+    # Hint 3
+    problem.addConstraint(AllDifferentConstraint(), ["Berlin", "Brandenburg"])
+    
+    # Hint 4
+    problem.addConstraint(AllDifferentConstraint(), ["Brandenburg", "Berlin", "Mecklenburg-Vorpommern", "Niedersachsen", "Sachsen-Anhalt", "Sachsen"])
+    
+    # Hint 5
+    problem.addConstraint(AllDifferentConstraint(), ["Bremen", "Niedersachsen"])
+    
+    # Hint 6
+    problem.addConstraint(AllDifferentConstraint(), ["Hamburg", "Niedersachsen", "Schleswig-Holstein"])
+    
+    # Hint 7
+    problem.addConstraint(AllDifferentConstraint(), ["Hessen", "Baden-Württemberg", "Bayern", "Rheinland-Pfalz", "Nordrhein-Westfalen", "Thüringen", "Niedersachsen"])
+    
+    # Hint 8
+    problem.addConstraint(AllDifferentConstraint(), ["Mecklenburg-Vorpommern", "Brandenburg", "Niedersachsen", "Schleswig-Holstein"])
+    
+    # Hint 9
+    problem.addConstraint(AllDifferentConstraint(), ["Niedersachsen", "Bremen", "Hamburg", "Nordrhein-Westfalen", "Mecklenburg-Vorpommern", "Sachsen-Anhalt", "Schleswig-Holstein", "Thüringen", "Hessen"])
+    
+    # Hint 10
+    problem.addConstraint(AllDifferentConstraint(), ["Nordrhein-Westfalen", "Hessen", "Niedersachsen", "Rheinland-Pfalz"])
+    
+    # Hint 11
+    problem.addConstraint(AllDifferentConstraint(), ["Rheinland-Pfalz", "Baden-Württemberg", "Hessen", "Nordrhein-Westfalen", "Saarland"])
+    
+    # Hint 12
+    problem.addConstraint(AllDifferentConstraint(), ["Saarland", "Rheinland-Pfalz"])
+    
+    # Hint 13
+    problem.addConstraint(AllDifferentConstraint(), ["Sachsen", "Bayern", "Thüringen", "Sachsen-Anhalt", "Brandenburg"])
+    
+    # Hint 14
+    problem.addConstraint(AllDifferentConstraint(), ["Sachsen-Anhalt", "Sachsen", "Thüringen", "Niedersachsen", "Brandenburg"])
+    
+    # Hint 15
+    problem.addConstraint(AllDifferentConstraint(), ["Schleswig-Holstein", "Hamburg", "Mecklenburg-Vorpommern", "Niedersachsen"])
+    
+    # Hint 16
+    problem.addConstraint(AllDifferentConstraint(), ["Thüringen", "Sachsen", "Sachsen-Anhalt", "Niedersachsen", "Hessen", "Bayern"])
+    
+    solution = problem.getSolution()
+    return solution
+    
+def showSolution(solution):
+    print("Solution:")
+    for state in solution:
+        print(state + ": " + str(solution[state]))
+    
+def main():
+    range = 8
+    showSolution(solve(range))
+    
+if __name__ == "__main__":
+    main()
--- 03_Aufgabe/readme.md
+++ 03_Aufgabe/readme.md
@@ -40,3 +40,44 @@ Ein weniger abstraktes Beispiel wären Lehrer welche in verschiedenen Klassenzim
     problem.addVariable(fächer, for i in range(1, 4))
     
 Den Lehrern und den Klassenzimmern werden hier jeweils die Räume (Wertebereich) 1 bis 3 zugeordnet.
+
+<br>
+
+### Erstellen von Constraints
+
+#### All Different Constraint
+
+Die python constraint library bietet die Möglichkeit ein Constraint für Variablen bei denen sich alle Werte unterscheiden müssen zu erstellen. Dieses Constraint kann mit dem Befehl `problem.addConstraint(AllDifferentConstraint())` erstellt werden. Dieses Constraint kann auch mit einer Liste von Variablen erstellt werden, in diesem Fall müssen sich die Werte der Variablen unterscheiden.
+
+    problem.addConstraint(AllDifferentConstraint(), ["var1", "var2"])
+    
+Für unser Lehrerbeispiel wäre das also:
+
+    problem.addConstraint(AllDifferentConstraint(), lehrer)
+    
+Das oben gezeigte Beispiel heißt, dass *alle Lehrer in unterschiedlichen Klassenzimmern unterrichten müssen*.
+
+#### Constraints mit Lambda Funktionen
+
+Es ist zudem möglich Constraints mittels Lambda Funktionen zu erstellen. Diese Funktionen müssen dann mit dem Befehl `problem.addConstraint()` hinzugefügt werden. Die Lambda Funktionen müssen dabei zwei Parameter enthalten, die die beiden Variablen sind, die miteinander verglichen werden sollen. Die Lambda Funktion muss dann `True` zurückgeben, wenn die beiden Variablen die Bedingung erfüllen und `False` wenn sie die Bedingung nicht erfüllen.
+
+    problem.addConstraint(lambda var1, var2: var1 != var2, ["var1", "var2"])
+    
+Für unser Lehrerbeispiel wäre das also:
+
+    problem.addConstraint(lambda lehrer1, fach1: lehrer1 != fach1, ["Herr Müller", "Deutsch"]])
+    
+Das oben gezeigte Beispiel heißt, dass *Herr Müller nicht Deutsch unterrichten darf*.
+
+
+#### Binäre (2 Stellige) Constraints
+
+Hängt eine Variable nur von einem Wert des Wertebereich ab, kann auch ein binäres Constraint erstellt werden.
+
+    problem.addConstraint(lambda var1: var1 != 1, ["var1"])
+
+Für unser Lehrerbeispiel wäre das also:
+
+    problem.addConstraint(lambda lehrer1: lehrer1 != 1, ["Herr Müller"])
+    
+Das oben gezeigte Beispiel heißt, dass *Herr Müller nicht im Raum 1 unterrichten darf*.
